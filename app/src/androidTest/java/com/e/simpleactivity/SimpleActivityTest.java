ae4948a1e2eaadaa3323f36d41a2015c79fbb7a8
rojinmiya <miyarojin557@gmail.com>
2019-06-02 12:06:44 +0545

    Update

--- app/src/androidTest/java/com/e/simpleactivity/SimpleActivityTest.java
+++ app/src/androidTest/java/com/e/simpleactivity/SimpleActivityTest.java
@@ -1,7 +1,9 @@
 package com.e.simpleactivity;
 
-import android.support.test.runner.AndroidJUnit4;
 
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
@@ -9,13 +11,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -33,7 +36,9 @@ public class SimpleActivityTest {
 
         onView(withId(R.id.btnClick)).perform(click());
 
-        onView(withId(R.id.tvDisplay)).check(matches(withText(mText)));
+        onView(withId(R.id.tvDisplay))
+                .check(matches(withText(mText)));
+
     }
 
 
